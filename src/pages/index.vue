9c701b3f0e909c1ac1f630e683e87936ff62fc2d
RecorderZero <ian03121997@gmail.com>
2024-05-03 23:44:49 +0800

    finish carousel location date

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,12 +4,29 @@
       <v-container v-if="!carouselItems">
           跑馬燈加載中
       </v-container>
-      <v-container v-else>
-      <v-carousel :cycle=true interval="3000">
-          <!-- 輪播內容 -->
-          <v-carousel-item v-for="(item, index) in carouselItems" :src="item" :key="index">
-          </v-carousel-item>
-      </v-carousel>
+      <v-container v-else fluid>
+    <v-layout class="row wrap justify-center">
+         <v-carousel :cycle=true interval="3000">
+          <v-carousel-item
+            v-for="(item,i) in carouselItems"
+            :key="i"  
+            :src="item.imageUrl"
+          >
+                    <v-container
+                      class="fill-height
+                      fluid
+                      pa-0 ma-0 pb-3" 
+                    >
+                      <v-layout class="fill-height align-end pb-4 mb-4">
+                          <v-card color="rgba(255,255,255,0.4)" elevation="0" class="pa-2">
+                          <v-icon>mdi-map-marker</v-icon><span class="headline black--text" v-text="item.location"></span><br />
+                          <v-icon>mdi-calendar</v-icon><span class="headline black--text" v-text="item.date"></span>
+                          </v-card>
+                      </v-layout>
+                    </v-container>
+             </v-carousel-item>
+           </v-carousel>
+      </v-layout>
     </v-container>
       <v-container>
           <h2>最新消息</h2>
@@ -100,7 +117,7 @@ export default {
   },
   created() {
     http.get('/get_valid_carousel?source=front')
-        .then(response => this.carouselItems = response.data.image_urls)
+        .then(response => this.carouselItems = response.data)
         .catch(error => {
           if (error.response) {
             // 在控制台顯示後端返回的詳細錯誤訊息
